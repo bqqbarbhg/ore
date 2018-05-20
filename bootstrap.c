284d40b547ccaaf323b3ce1600f0b4e2ef5ee33e
bqqbarbhg <samuli.1995@hotmail.com>
2018-05-20 20:28:04 +0300

    Add expression parsing

--- bootstrap.c
+++ bootstrap.c
@@ -172,9 +172,17 @@ enum {
 	tok_le,
 	tok_ge,
 	tok_or,
+	tok_and,
 
-	tok_kw_if = 2000,
+	tok_op_assign = 2000,
+	tok_add_assign = tok_op_assign + '+',
+	tok_sub_assign = tok_op_assign + '-',
+	tok_mul_assign = tok_op_assign + '*',
+	tok_div_assign = tok_op_assign + '/',
+
+	tok_kw_if = 3000,
 	tok_kw_else,
+	tok_kw_while,
 	tok_kw_def,
 	tok_kw_val,
 	tok_kw_var,
@@ -189,7 +197,6 @@ enum {
 	tok_kw_new,
 	tok_kw_impl,
 	tok_kw_extends,
-	tok_kw_sizeof,
 };
 
 typedef struct token {
@@ -287,9 +294,9 @@ struct ast {
 		} def;
 
 		struct {
-			ast *cond;
-			ast *true_stmt;
-			ast *false_stmt;
+			ast **conds;
+			ast **bodies;
+			ast *else_body;
 		} if_;
 
 		struct {
@@ -321,13 +328,13 @@ struct ast {
 		} vardecl;
 
 		struct {
-			ast *lhs;
-			ast *rhs;
+			token op;
+			ast *left, *right;
 		} assign;
 
 		struct {
 			ast *left;
-			ast *name;
+			token name;
 		} member;
 
 		struct {
@@ -430,8 +437,7 @@ void find_line_col(file *f, unsigned pos, unsigned *line, unsigned *col) {
 		if (f->linebreaks[it] < pos) {
 			first = it + 1;
 			count -= step + 1;
-		}
-		else {
+		} else {
 			count = step;
 		}
 	}
@@ -641,8 +647,11 @@ void lex(compiler_state *s) {
 		s->token.kind = tok_end;
 		goto accept;
 
-	case '+': case '-': case '*': case '/': case '.':
-	case '(': case ')': case ':': case ',': case '{': case '}': case '[': case ']':
+	case '+': case '-': case '*': case '/':
+		s->token.kind = la == '=' ? eat(s), tok_op_assign + c : c;
+		goto accept;
+
+	case '.': case '(': case ')': case ':': case ',': case '{': case '}': case '[': case ']':
 		s->token.kind = c;
 		goto accept;
 
@@ -664,6 +673,10 @@ void lex(compiler_state *s) {
 
 	case '|':
 		s->token.kind = la == '|' ? eat(s), tok_or : '|';
+		goto accept;
+
+	case '&':
+		s->token.kind = la == '&' ? eat(s), tok_and : '&';
 		goto accept;
 
 	case '\n':
@@ -698,6 +711,8 @@ void unlex(compiler_state *s) {
 }
 
 inl bool accept(compiler_state *s, unsigned kind) {
+	if (s->error) return false;
+
 	if (s->token.kind == kind) {
 		lex(s);
 		return true;
@@ -756,8 +771,377 @@ ast *parse_name(compiler_state *s) {
 	}
 }
 
+ast *parse_statement(compiler_state *s);
+ast *parse_expr(compiler_state *s);
+ast *parse_type(compiler_state *s);
+
+ast *parse_expr_atom(compiler_state *s) {
+	ast *a = NULL;
+
+	lex(s);
+	switch (s->prev_token.kind) {
+
+	case tok_identifier:
+		a = push_ast(s, ast_identifier);
+		a->identifier.name = s->prev_token;
+		break;
+
+	case tok_number:
+		a = push_ast(s, ast_number);
+		a->number.value = s->prev_token;
+		break;
+
+	case '(':
+		a = push_ast(s, ast_paren);
+		a->paren.expr = parse_expr(s);
+		require(s, ')', "Expected a closing ')'");
+		break;
+
+	case '{':
+		a = push_ast(s, ast_block);
+		while (accept(s, '\n')) { }
+		while (!accept(s, '}') && !s->error) {
+			ast *st = parse_statement(s);
+			if (st != NULL) {
+				buf_push(a->block.stmts, st);
+			} else if (!s->error) {
+				error_at(s, s->prev_token, "Expected a statement");
+			}
+			while (accept(s, '\n')) { }
+		}
+		break;
+
+	default:
+		error_at(s, s->prev_token, "Expected an expression");
+		return NULL;
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_suffix(compiler_state *s) {
+	ast *a = parse_expr_atom(s);
+
+	while (!s->error) {
+		lex(s);
+		switch (s->prev_token.kind) {
+
+		case '.': {
+			ast *op = push_ast(s, ast_member);
+			op->member.left = a;
+			require(s, tok_identifier, "Expected a member name after '.'");
+			op->member.name = s->prev_token;
+			a = op;
+		} break;
+
+		case '[': {
+			ast *op = push_ast(s, ast_generic);
+			op->generic.name = a;
+
+			if (s->token.kind == ']') {
+				error_at(s, s->token, "Generic parameter list cannot be empty");
+				return NULL;
+			}
+
+			do {
+				ast *arg = parse_type(s);
+				buf_push(op->generic.args, arg);
+			} while (accept(s, ','));
+
+			require(s, ']', "Expected closing ']' for generic parameter list");
+				
+			a = op;
+		} break;
+
+		case '(': {
+			ast *op = push_ast(s, ast_call);
+			op->call.left = a;
+
+			if (!accept(s, ')')) {
+				do {
+					ast *arg = parse_expr(s);
+					buf_push(op->call.args, arg);
+				} while (accept(s, ','));
+
+				require(s, ')', "Expected closing ')' for function call");
+			}
+
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_prefix(compiler_state *s) {
+	ast *a = NULL;
+
+	lex(s);
+	switch (s->prev_token.kind) {
+	case '!':
+		a = push_ast(s, ast_unop);
+		a->unop.op = s->prev_token;
+		a->unop.arg = parse_expr_suffix(s);
+		break;
+
+	default:
+		unlex(s);
+		a = parse_expr_suffix(s);
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_1_mul(compiler_state *s) {
+	ast *a = parse_expr_prefix(s);
+
+	while (!s->error) {
+		lex(s);
+		switch (s->prev_token.kind) {
+
+		case '*': case '/': case '%': {
+			ast *op = push_ast(s, ast_binop);
+			op->binop.op = s->prev_token;
+			op->binop.left = a;
+			op->binop.right = parse_expr_prefix(s);
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_2_add(compiler_state *s) {
+	ast *a = parse_expr_1_mul(s);
+
+	while (!s->error) {
+		lex(s);
+		switch (s->prev_token.kind) {
+
+		case '+': case '-': {
+			ast *op = push_ast(s, ast_binop);
+			op->binop.op = s->prev_token;
+			op->binop.left = a;
+			op->binop.right = parse_expr_1_mul(s);
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_3_cmp(compiler_state *s) {
+	ast *a = parse_expr_2_add(s);
+
+	while (!s->error) {
+		lex(s);
+		switch (s->prev_token.kind) {
+
+		case tok_eq: case tok_not_eq: case tok_ge: case tok_le:
+		case '<': case '>': {
+			ast *op = push_ast(s, ast_binop);
+			op->binop.op = s->prev_token;
+			op->binop.left = a;
+			op->binop.right = parse_expr_2_add(s);
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_4_and(compiler_state *s) {
+	ast *a = parse_expr_3_cmp(s);
+
+	while (!s->error) {
+		lex(s);
+		switch (s->prev_token.kind) {
+		case tok_and: {
+			ast *op = push_ast(s, ast_binop);
+			op->binop.op = s->prev_token;
+			op->binop.left = a;
+			op->binop.right = parse_expr_3_cmp(s);
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *parse_expr_5_or(compiler_state *s) {
+	ast *a = parse_expr_4_and(s);
+
+	while (!s->error) {
+
+		lex(s);
+		switch (s->prev_token.kind) {
+		case tok_or: {
+			ast *op = push_ast(s, ast_binop);
+			op->binop.op = s->prev_token;
+			op->binop.left = a;
+			op->binop.right = parse_expr_4_and(s);
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
+}
+
 ast *parse_expr(compiler_state *s) {
-	return NULL;
+
+	ast *a = NULL;
+
+	lex(s);
+	switch (s->prev_token.kind) {
+	
+	case tok_kw_if: {
+		ast *cond, *body;
+		a = push_ast(s, ast_if);
+		require(s, '(', "Expected '(' for if condition");
+		cond = parse_expr(s);
+		buf_push(a->if_.conds, cond);
+		require(s, ')', "Expected closing ')' for if condition");
+		while (accept(s, '\n')) { }
+		body = parse_statement(s);
+		buf_push(a->if_.bodies, body);
+
+another_elseif:
+		if (accept(s, tok_kw_else)) {
+			while (accept(s, '\n')) { }
+			if (accept(s, tok_kw_if)) {
+				require(s, '(', "Expected '(' for else if condition");
+				cond = parse_expr(s);
+				buf_push(a->if_.conds, cond);
+				require(s, ')', "Expected closing ')' for else if condition");
+				while (accept(s, '\n')) { }
+				body = parse_statement(s);
+				buf_push(a->if_.bodies, body);
+				goto another_elseif;
+			} else {
+				a->if_.else_body = parse_statement(s);
+			}
+		}
+	} break;
+
+	case tok_kw_while: {
+		a = push_ast(s, ast_while);
+		require(s, '(', "Expected '(' for while condition");
+		a->while_.cond = parse_expr(s);
+		require(s, ')', "Expected closing ')' for while condition");
+		a->while_.body = parse_statement(s);
+	} break;
+
+	case tok_kw_new: {
+		a = push_ast(s, ast_new);
+
+		if (s->token.kind != '{') {
+			a->new_.type = parse_type(s);
+		}
+
+		if (accept(s, '{')) {
+			if (s->token.kind != '}') {
+				do {
+					ast *arg = parse_expr(s);
+
+					if (accept(s, '=')) {
+						ast *name = arg;
+						arg = push_ast(s, ast_assign);
+						arg->assign.op = s->prev_token;
+						arg->assign.left = name;
+						arg->assign.right = parse_expr(s);
+					}
+
+					buf_push(a->new_.args, arg);
+				} while (accept(s, ','));
+			}
+
+			require(s, '}', "Expected closing '}' for initailizer block");
+		} else {
+			// TODO: Constructors
+		}
+
+	} break;
+
+	default:
+		unlex(s);
+		a = parse_expr_5_or(s);
+	}
+
+	return s->error ? NULL : a;
+}
+
+ast *finish_type_name(compiler_state *s, token name) {
+	ast *a = push_ast(s, ast_identifier);
+	a->identifier.name = s->prev_token;
+
+	while (!s->error) {
+		lex(s);
+		switch (s->prev_token.kind) {
+
+		case '.': {
+			ast *op = push_ast(s, ast_member);
+			op->member.left = a;
+			require(s, tok_identifier, "Expected a member name after '.'");
+			op->member.name = s->prev_token;
+			a = op;
+		} break;
+
+		case '[': {
+			ast *op = push_ast(s, ast_generic);
+			op->generic.name = a;
+
+			if (s->token.kind == ']') {
+				error_at(s, s->token, "Generic parameter list cannot be empty");
+				return NULL;
+			}
+
+			do {
+				ast *arg = parse_type(s);
+				buf_push(op->generic.args, arg);
+			} while (accept(s, ','));
+
+			require(s, ']', "Expected closing ']' for generic parameter list");
+				
+			a = op;
+		} break;
+
+		default:
+			unlex(s);
+			return a;
+		}
+	}
+
+	return s->error ? NULL : a;
 }
 
 ast *parse_type(compiler_state *s) {
@@ -772,25 +1156,7 @@ ast *parse_type(compiler_state *s) {
 		break;
 
 	case tok_identifier:
-		a = push_ast(s, ast_identifier);
-		a->identifier.name = s->prev_token;
-		while (accept(s, '[')) {
-			ast *gen = push_ast(s, ast_generic);
-			gen->generic.name = a;
-
-			if (s->token.kind == ']') {
-				error_at(s, s->token, "Generic argument list cannot be empty");
-				return NULL;
-			}
-
-			do {
-				ast *arg = parse_type(s);
-				buf_push(gen->generic.args, arg);
-			} while (accept(s, ','));
-
-			require(s, ']', "Expected closing ']' after generic argument list");
-			a = gen;
-		}
+		a = finish_type_name(s, s->prev_token);
 		break;
 
 	default:
@@ -933,6 +1299,13 @@ ast *parse_free_statement(compiler_state *s) {
 		if (accept(s, ':')) {
 			a->def.ret = parse_type(s);
 		}
+
+		if (accept(s, '=')) {
+			a->def.body = parse_expr(s);
+		} else if (s->token.kind == '{') {
+			a->def.body = parse_expr(s);
+		}
+
 		break;
 
 	case '{':
@@ -965,17 +1338,44 @@ ast *parse_statement(compiler_state *s) {
 	lex(s);
 	switch (s->prev_token.kind) {
 
-	case tok_kw_if:
+	case '{':
+		a = push_ast(s, ast_block);
+		while (accept(s, '\n')) { }
+		while (!accept(s, '}') && !s->error) {
+			ast *st = parse_statement(s);
+			if (st != NULL) {
+				buf_push(a->block.stmts, st);
+			} else if (!s->error) {
+				error_at(s, s->prev_token, "Expected a statement");
+			}
+			while (accept(s, '\n')) { }
+		}
 		break;
 
 	default:
+		unlex(s);
 		a = parse_free_statement(s);
 		if (a == NULL && !s->error) {
 			a = parse_expr(s);
+
+			lex(s);
+			switch (s->prev_token.kind) {
+			case '=':
+			case tok_add_assign: case tok_sub_assign:
+			case tok_mul_assign: case tok_div_assign: {
+				ast *left = a;
+				a = push_ast(s, ast_assign);
+				a->assign.op = s->prev_token;
+				a->assign.left = left;
+				a->assign.right = parse_expr(s);
+			} break;
+
+			default:
+				unlex(s);
+			}
 		}
 
 	}
-
 
 	return s->error ? NULL : a;
 }
@@ -1064,8 +1464,14 @@ void dump_ast_type(FILE *f, ast *a) {
 		fputs(a->identifier.name.str, f);
 		break;
 
+	case ast_member:
+		dump_ast_type(f, a->member.left);
+		putc('.', f);
+		fputs(a->member.name.str, f);
+		break;
+
 	case ast_generic:
-		dump_ast_name(f, a->generic.name);
+		dump_ast_type(f, a->generic.name);
 		fputc('[', f);
 		for (unsigned i = 0; i < buf_len(a->generic.args); i++) {
 			if (i != 0) fputs(", ", f);
@@ -1098,6 +1504,142 @@ void dump_indent(FILE *f, int indent) {
 	}
 }
 
+void dump_ast_free_statement(FILE *f, int indent, ast* a);
+
+void dump_ast_expr(FILE *f, int indent, ast* a) {
+	switch (a->kind) {
+
+	case ast_block:
+		fputs("{\n", f);
+		indent++;
+
+		for (unsigned i = 0; i < buf_len(a->block.stmts); i++) {
+			dump_indent(f, indent);
+			dump_ast_expr(f, indent, a->block.stmts[i]);
+			putc('\n', f);
+		}
+		indent--;
+
+		dump_indent(f, indent);
+		putc('}', f);
+		break;
+
+	case ast_if:
+
+		for (unsigned i = 0; i < buf_len(a->if_.conds); i++) {
+			if (i > 0) {
+				putc('\n', f);
+				dump_indent(f, indent);
+				fputs("else ", f);
+			}
+			fputs("if (", f);
+			dump_ast_expr(f, indent, a->if_.conds[i]);
+			fputs(") ", f);
+			dump_ast_expr(f, indent, a->if_.bodies[i]);
+		}
+
+		if (a->if_.else_body) {
+			putc('\n', f);
+			dump_indent(f, indent);
+			fputs("else ", f);
+			dump_ast_expr(f, indent, a->if_.else_body);
+		}
+
+		break;
+
+	case ast_while:
+		fputs("while (", f);
+		dump_ast_expr(f, indent, a->while_.cond);
+		fputs(") ", f);
+		dump_ast_expr(f, indent, a->while_.body);
+		break;
+
+	case ast_paren:
+		putc('(', f);
+		dump_ast_expr(f, indent, a->paren.expr);
+		putc(')', f);
+		break;
+
+	case ast_binop:
+		dump_ast_expr(f, indent, a->binop.left);
+		putc(' ', f);
+		fputs(a->binop.op.str, f);
+		putc(' ', f);
+		dump_ast_expr(f, indent, a->binop.right);
+		break;
+
+	case ast_assign:
+		dump_ast_expr(f, indent, a->assign.left);
+		putc(' ', f);
+		fputs(a->assign.op.str, f);
+		putc(' ', f);
+		dump_ast_expr(f, indent, a->assign.right);
+		break;
+
+	case ast_unop:
+		fputs(a->unop.op.str, f);
+		dump_ast_expr(f, indent, a->unop.arg);
+		break;
+
+	case ast_identifier:
+		fputs(a->identifier.name.str, f);
+		break;
+
+	case ast_number:
+		fputs(a->number.value.str, f);
+		break;
+
+	case ast_generic:
+		dump_ast_expr(f, indent, a->generic.name);
+		fputc('[', f);
+		for (unsigned i = 0; i < buf_len(a->generic.args); i++) {
+			if (i != 0) fputs(", ", f);
+			dump_ast_type(f, a->generic.args[i]);
+		}
+		fputc(']', f);
+		break;
+
+	case ast_call:
+		dump_ast_expr(f, indent, a->call.left);
+		fputc('(', f);
+		for (unsigned i = 0; i < buf_len(a->call.args); i++) {
+			if (i != 0) fputs(", ", f);
+			dump_ast_expr(f, indent, a->call.args[i]);
+		}
+		fputc(')', f);
+		break;
+
+	case ast_member:
+		dump_ast_expr(f, indent, a->member.left);
+		putc('.', f);
+		fputs(a->member.name.str, f);
+		break;
+
+	case ast_new:
+		fputs("new", f);
+		if (a->new_.type) {
+			fputc(' ', f);
+			dump_ast_type(f, a->new_.type);
+		}
+
+		if (a->new_.args) {
+			fputs(" { ", f);
+			for (unsigned i = 0; i < buf_len(a->new_.args); i++) {
+				if (i != 0) {
+					fputs(", ", f);
+				}
+
+				dump_ast_expr(f, indent, a->new_.args[i]);
+			}
+			fputs(" }", f);
+		}
+		break;
+
+	default:
+		dump_ast_free_statement(f, indent, a);
+	}
+}
+
 void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 	switch (a->kind) {
 
@@ -1108,6 +1650,7 @@ void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 		for (unsigned i = 0; i < buf_len(a->block.stmts); i++) {
 			dump_indent(f, indent);
 			dump_ast_free_statement(f, indent, a->block.stmts[i]);
+			putc('\n', f);
 			putc('\n', f);
 		}
 		indent--;
@@ -1120,6 +1663,10 @@ void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 		dump_ast_mods(f, a->typedecl.mods);
 		fputs("type ", f);
 		dump_ast_name(f, a->typedecl.name);
+		if (a->typedecl.init) {
+			fputs(" = ", f);
+			dump_ast_type(f, a->typedecl.init);
+		}
 		break;
 
 	case ast_vardecl:
@@ -1129,9 +1676,14 @@ void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 			fputs(": ", f);
 			dump_ast_type(f, a->vardecl.type);
 		}
+		if (a->vardecl.init) {
+			fputs(" = ", f);
+			dump_ast_expr(f, indent, a->vardecl.init);
+		}
 		break;
 
 	case ast_def:
+		dump_ast_mods(f, a->def.mods);
 		fputs("def ", f);
 		dump_ast_name(f, a->def.name);
 		if (!a->def.no_parens) {
@@ -1145,6 +1697,10 @@ void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 		if (a->def.ret) {
 			fputs(": ", f);
 			dump_ast_type(f, a->def.ret);
+		}
+		if (a->def.body) {
+			fputs(" = ", f);
+			dump_ast_expr(f, indent, a->def.body);
 		}
 		break;
 
@@ -1165,6 +1721,7 @@ void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 		indent++;
 
 		for (unsigned i = 0; i < buf_len(a->type.stmts); i++) {
+			if (i != 0) putc('\n', f);
 			dump_indent(f, indent);
 			dump_ast_free_statement(f, indent, a->type.stmts[i]);
 			putc('\n', f);
@@ -1173,9 +1730,7 @@ void dump_ast_free_statement(FILE *f, int indent, ast* a) {
 		indent--;
 		dump_indent(f, indent);
 		putc('}', f);
-
 		break;
-
 
 	default:
 		assert(0 && "Unexpected AST type");
@@ -1204,6 +1759,7 @@ void dump_ast_toplevel(FILE *f, int indent, ast *a) {
 
 void dump_ast(FILE *f, ast** a) {
 	for (unsigned i = 0; i < buf_len(a); i++) {
+		if (i != 0) putc('\n', f);
 		dump_ast_toplevel(f, 0, a[i]);
 		putc('\n', f);
 	}
@@ -1223,6 +1779,7 @@ void init_compiler(compiler_state *s) {
 
 	add_keyword(s, "if", tok_kw_if);
 	add_keyword(s, "else", tok_kw_else);
+	add_keyword(s, "while", tok_kw_while);
 	add_keyword(s, "def", tok_kw_def);
 	add_keyword(s, "val", tok_kw_val);
 	add_keyword(s, "var", tok_kw_var);
@@ -1238,7 +1795,6 @@ void init_compiler(compiler_state *s) {
 	add_keyword(s, "new", tok_kw_new);
 	add_keyword(s, "impl", tok_kw_impl);
 	add_keyword(s, "extends", tok_kw_extends);
-	add_keyword(s, "sizeof", tok_kw_sizeof);
 }
 
 int main(int argc, char **argv) {
